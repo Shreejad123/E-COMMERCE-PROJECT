04318e8d125337ee83ac3b834f282e26634766fb
https://github.com/Shreejad123 <shreejadkotian00@gmail.com>
2026-02-09 18:24:55 +0530

    Added toast notifications

---
+++ App/src/pages/toastmessage.jsx
@@ -0,0 +1,19 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const ToastProvider = () => {
+  return (
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      closeOnClick
+      pauseOnHover
+      draggable
+      theme="light"
+      style={{ zIndex: 9999 }}
+    />
+  );
+};
+
+export default ToastProvider;
--- App/src/pages/viewDetails.jsx
+++ App/src/pages/viewDetails.jsx
@@ -4,14 +4,21 @@ import { FaRegHeart } from "react-icons/fa";
 import styles from "./viewDetails.module.css";
 import { IoCartSharp } from "react-icons/io5";
 import { RiFeedbackFill } from "react-icons/ri";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ToastProvider from "./toastmessage";
 
 const ViewDetails = () => {
   const { id } = useParams();
   const [products, setProducts] = useState(null);
   const handleClick = () => {
-    console.log("clicked");
+    toast.success("Added to cart!", { autoClose: 10000 });
+    console.log("cliked on toast");
   };
-
+  const showToastMessage = () => {
+    toast.success("Added to Wishlist!", { autoClose: 10000 });
+    console.log("cliked on toast");
+  };
   useEffect(() => {
     fetch(`https://fakestoreapi.com/products/${id}`)
       .then((res) => {
@@ -31,19 +38,21 @@ const ViewDetails = () => {
   return (
     <div>
       <div className={styles.card}>
+        <h5>{products.title}</h5>
+
         <div>
           <img
-            className={styles.image}
+            className={styles.imageSection}
             src={products.image}
             alt={products.title}
           />
         </div>
         <div className={styles.section}>
-          <h5>{products.title}</h5>
           <div className={styles.paragraph}>
             <p>${products.price}</p>
-            <p>Ratings:{products.rating.rate}⭐</p>
+            <p>{products.rating.rate}⭐</p>
             <p>Category:{products.category}</p>
+            <p>{products.description}</p>
           </div>
           <button
             className={`btn btn-primary ${styles.customBtn}`}
@@ -52,13 +61,17 @@ const ViewDetails = () => {
             <IoCartSharp size={20} />
             Add to cart
           </button>
-          <button className={`btn btn-danger ${styles.customBtn}`}>
+          <button
+            className={`btn btn-danger ${styles.customBtn}`}
+            onClick={showToastMessage}
+          >
             <FaRegHeart size={20} /> Add to wishlist
           </button>
           <button className={`btn btn-success ${styles.customBtn}`}>
             <RiFeedbackFill size={20} />
             Rate Now
           </button>
+          <ToastProvider></ToastProvider>
         </div>
       </div>
     </div>
